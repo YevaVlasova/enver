11e0bea7eb1bf37e92af4bac29cf92ab02568e05
YevaVlasova <eva-krechetova@ukr.net>
2023-05-09 17:02:43 +0300

    adaptability for small mobile

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
+// Menu Burger //
+
 function navToggle() {
-	var links = document.getElementById('navUl');
-	links.classList.toggle("_active");
+	document.getElementById('navUl').classList.toggle("_active");
+	document.getElementById('body').classList.toggle("overflow-hidden");
+	document.getElementById('html').classList.toggle("overflow-hidden");
 }
 
+// Spinner //
 
-// footer toggle
-
-function infoToggle() {
-    var footerInfo = document.getElementById("footerInfo");
-    footerInfo.classList.toggle("db");
+function checkSpinner() {
+	document.getElementById('spinner').classList.add("_active");
+    setTimeout(() => {
+        document.getElementById('spinner').classList.remove("_active");
+        document.getElementById('desc').innerHTML = 'Thanks for watching! We hope you enjoyed this video. If you have any questions, we are always in touch.';
+    }, 3000);
 }
 
-
-function contactsToggle() {
-    var footerContacts = document.getElementById("footerContacts");
-    footerContacts.classList.toggle("db");
-}
-
-function hourToggle() {
-    var footerHours = document.getElementById("footerHours");
-    footerHours.classList.toggle("db");
-}
-
+// Mouse move effect //
 
 document.getElementById("cards").onmousemove = e => {
     for(const card of document.getElementsByClassName("service-item")) {
@@ -34,10 +29,33 @@ document.getElementById("cards").onmousemove = e => {
     }
 }
 
+// Owl slider //
 
 $('.owl-carousel').owlCarousel({
     items:3,
     lazyLoad:true,
     loop:false,
-    margin:24
+    margin:24,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            dots:false
+        },
+        592:{
+            items:2,
+            nav:false
+        },
+        991:{
+            items:3,
+            nav:false,
+            loop:false
+        }
+    }
 });
+
+// Current year //
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
